6003e1d4143eb55a28325bd2e37dbc4a887b2844
2369765786@qq.com <2369765786@qq.com>
2020-11-29 20:11:26 +0800

    更新版

--- week01/var/log/python-11-26/log.py
+++ week01/var/log/python-11-26/log.py
@@ -1,11 +1,23 @@
 import logging
+import os
+import time
 def callback():
-    print('你被调用了！')
-callback()
-logging.basicConfig(filename='test.log',level=logging.DEBUG,datefmt='%Y-%M-%D %H:%M:%S',format='%(asctime)s %(name)-8s %(levelname)-8s[line:%(lineno)d]%(message)s')
-logging.debug('debug message')
-'''logging.info('info message')
-logging.warning('warning message')
-logging.error('error message')
-logging.critical('critical message')'''
+    current_day=time.strftime('Y%-m%-d%',time.localtime())
+    path='/c/Users/zhaoliang/var/log/python-'+str(current_day)
+    os.chdir(path)
+    if os.path.exists(path):
+        os.chdir(path)
+    with open('test.log','w+')as f:
+        f.write(current_day)
+
+    logging.basicConfig(filename='test.log',level=logging.DEBUG,datefmt='%Y-%M-%D %H:%M:%S',
+    format='%(asctime)s %(name)-8s %(levelname)-8s[line:%(lineno)d]%(message)s')
+    logging.debug('debug message')
+    logging.info('info message')
+    logging.warning('warning message')
+    logging.error('error message')
+    logging.critical('critical message')
+if __name__ == "__main__":
+    callback()
+    
 
